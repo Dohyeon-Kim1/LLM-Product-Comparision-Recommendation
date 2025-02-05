cf585f955dbcc645bbfe7c28c3dc97c4d26e717d
Dohyeon-Kim1 <dhkim011030@gmail.com>
2025-02-05 17:09:08 +0900

    [upload] models.py

---
+++ models.py
@@ -0,0 +1,31 @@
+import os
+import torch
+from transformers import pipeline
+from unsloth import FastLanguageModel
+
+
+def unsloth_model(args):
+    model, tokenizer = FastLanguageModel(
+        model_name="google/gemma-2-9b-it",
+        load_in_4bit=True,
+        max_seq_length=2048
+    )
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r=args.r,
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.dropout   #과적합 방지, 일반화 성능 향상
+    )
+    return model, tokenizer
+
+
+def generation_pipe(args):
+    pipe = pipeline(
+        task="text-generateion",
+        model=args.model_id,
+        torch_dtype=torch.float16,
+        device_map="cuda" if torch.cuda.is_available() else "cpu",
+        token=os.environ["HF_TOKEN"]
+    )
+    return pipe
